c0f7f2192f6c7e6cd2954703dd7ce21337ac9fe4
Nockda <72916112+Nockda@users.noreply.github.com>
2023-01-03 14:38:36 +0000

    Finish House App

--- airbnb-clone-backend/houses/models.py
+++ airbnb-clone-backend/houses/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class House(models.Model):
+    
+    """Model definition for houses"""
+    
+    name = models.CharField(max_length=140)
+    price_per_night = models.PositiveIntegerField()
+    description = models.TextField()
+    address = models.CharField(max_length=140)
+    pet_allowed = models.BooleanField(default=True, 
+    help_text="Does this house allowed pets?", verbose_name="Pets Allowed")
+
+    def __str__(self):
+        return self.name
